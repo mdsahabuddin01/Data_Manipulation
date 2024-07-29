aea971a78f0ec806c4a3ed8e7f78a9297e1ae801
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2024-07-29 14:06:30 +0100

    Create windows_func.sql

---
+++ windows_func.sql
@@ -0,0 +1,26 @@
+/*
+The OVER() clause allows you to pass an aggregate function down a data set, similar to subqueries in SELECT. The OVER() clause offers significant benefits over subqueries in select -- namely, your queries will run faster, and the OVER() clause has a wide range of additional functions and clauses you can include with it that we will cover later on in this chapter.
+
+In this exercise, you will revise some queries from previous chapters using the OVER() clause.
+
+Instructions
+100 XP
+Select the match ID, country name, season, home, and away goals from the match and country tables.
+Complete the query that calculates the average number of goals scored overall and then includes the aggregate value in each row using a window function.
+
+
+
+*/
+
+SELECT 
+	-- Select the id, country name, season, home, and away goals
+	m.id, 
+    c.name AS country, 
+    m.season,
+	m.home_goal,
+	m.away_goal,
+    -- Use a window to include the aggregate average in each row
+	avg(m.home_goal + m.away_goal) over() AS overall_avg
+FROM match AS m
+LEFT JOIN country AS c ON m.country_id = c.id;
+
