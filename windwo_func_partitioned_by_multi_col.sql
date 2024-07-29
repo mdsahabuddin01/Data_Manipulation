fa8c22edf2552b43ab7379e2b8d49eba4b7896a4
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2024-07-29 19:47:40 +0100

    windwo_func_partitioned_by_multi_col.sql

---
+++ windwo_func_partitioned_by_multi_col.sql
@@ -0,0 +1,30 @@
+/*
+PARTITION BY multiple columns
+The PARTITION BY clause can be used to break out window averages by multiple data points (columns). You can even calculate the information you want to use to partition your data! For example, you can calculate average goals scored by season and by country, or by the calendar year (taken from the date column).
+
+In this exercise, you will calculate the average number home and away goals scored Legia Warszawa, and their opponents, partitioned by the month in each season.
+
+Instructions
+100 XP
+Construct two window functions partitioning the average of home and away goals by season and month.
+Filter the dataset by Legia Warszawa's team ID (8673) so that the window calculation only includes matches involving them.
+*/
+
+SELECT 
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+         ELSE 'away' END AS warsaw_location,
+	-- Calculate average goals partitioned by season and month
+    avg(home_goal) over(partition by season, 
+         	EXTRACT(month FROM date)) AS season_mo_home,
+    avg(away_goal) over(partition by season, 
+            EXTRACT(month FROM date)) AS season_mo_away
+FROM match
+WHERE 
+	hometeam_id = 8673
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
+
